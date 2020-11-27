44c6cfef4bd36d4d29c7c5f45982adf93ac6f631
Rachel <therara05@gmail.com>
2020-11-27 11:49:29 -0500

    i found an item list online and used it work a dropped items= type thing, i also added a function for that DROPS items/purges the inventory

--- turtle-mine/mine.lua
+++ turtle-mine/mine.lua
@@ -11,3 +11,39 @@ else
   print('improbable size  given,, now defulting to 10x10x2')
 end
 --for the size of dig
+
+dropped_items = {
+  "minecraft:stone",
+  "minecraft:dirt",
+  "minecraft:cobblestone",
+  "minecraft:sand",
+  "minecraft:gravel",
+  "minecraft:redstone",
+  "minecraft:flint",
+  "railcraft:ore_metal",
+  "extrautils2:ingredients",
+  "minecraft:dye",
+  "thaumcraft:nugget",
+  "thaumcraft:crystal_essence",
+  "thermalfoundation:material",
+  "projectred-core:resource_item",
+  "deepresonance:resonating_ore",
+}
+--ngl i looked this list of items up,,, check this later
+
+function dropItems()
+  print("clearing inventory,,,")
+  for slot = 1, slot_count, do 1
+    local item = turtle.getItemDetail(slot)
+    if(item == nil)then
+      for filterIndex = 1, #dropped_items, do 1
+        if(item['name'] == dropped_items,v[filterIndex]) then
+          print('dropping-'... item['name'])
+          turtle.select(slot)
+          turtle.dropDown()
+        end
+      end
+    end
+  end
+end
+--drops items/purges inventory
