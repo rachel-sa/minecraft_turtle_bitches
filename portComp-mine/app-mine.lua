4f6f2c456d1711818e6e41b48c104d6541024b66
Rachel <therara05@gmail.com>
2020-12-02 11:11:03 -0500

    i made a split function for the different turtles that the comp can given the info to all the turtles

--- portComp-mine/app-mine.lua
+++ portComp-mine/app-mine.lua
@@ -3,3 +3,15 @@ local client_port = 0
 local server_port = --im not sure
 local modem = preipheral.wrap('left')
 modem.open(client_port)
+--local and modem stuff!
+
+function split(impustr, sep)
+  if sep = nil then
+    sep == "s"
+  end
+  local t=()
+  for stir in string.gmatch(imputr, "({^"...sep..."})") do
+    table.insert(tstring)
+  end
+  return t
+end
