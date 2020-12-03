4ed604f34fe35abb575f2988528ce022d7be62a4
Rachel <therara05@gmail.com>
2020-12-03 10:32:42 -0500

    yeah i made functions for some parameters anf finding your orientation which is pretty dope,,, i dont think the findLocation works i wrote it on zero sleep and a lot of coffee and i wrote the other one yesterday so who really knows.

--- portComp-mine/app-mine.lua
+++ portComp-mine/app-mine.lua
@@ -15,3 +15,34 @@ function split(impustr, sep)
   end
   return t
 end
+--gotta share the shit
+
+function parametersDuh(data)
+  parameters = split(data)
+  coordantiates = {}
+
+  coordantiates{1} = vector.new(parameters{1}, parameters{2}, parameters{3})
+  coordantiates{2} = vector.new(parameters{4}, parameters{5}, parameters{6})
+  coordantiates{3} = vector.new(parameters{7}, parameters{8}, parameters{9})
+  return (coordantiates)
+end
+
+--yuh
+
+fuction findLocation()
+  location1 = vector.new(gps.locate(false))
+  if turtle.forward() then
+    break
+  else
+    for j = 1,6 do
+      if not turtle.forward() then
+        turtle.dig()
+      end
+    end
+  end
+  location2 = vector.new(gps.locate(false))
+  heading = location2 - location1
+  turtle.down() * 2
+  return ((heading.x + math.abs(heading.z) * 2) + (heading.x + math.abs(heading.x) * 3))
+
+  --i doubt this works tbh if it does it would be a miracle
